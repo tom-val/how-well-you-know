7c7815e1bf0d48100e75f625fb26d7b609dc6b6f
Tom <otasssss@gmail.com>
2021-10-10 14:22:07 +0300

    Refactor Answer service logic to domain level

--- HowWellYouKnow.API/HowWellYouKnow.Domain/Models/Guess.cs
+++ HowWellYouKnow.API/HowWellYouKnow.Domain/Models/Guess.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Text;
 
 namespace HowWellYouKnow.Domain.Models
 {
     public class Guess : BaseEntity
     {
+        private Guess() { }
         public Guid QuestionId { get; set; }
         public Question Question { get; set; }
         public List<QuestionVariant> QuestionVariants { get; set; }
@@ -13,5 +16,28 @@ namespace HowWellYouKnow.Domain.Models
         public Guid UserId { get; set; }
         public User GuessUser { get; set; }
         public Guid GuessUserId { get; set; }
+
+        public static Guess Create(Guid guessUserId, List<Guid> questionVariantIds, Guid userId, Question question)
+        {
+            if (!question.MultipleAnswers && questionVariantIds.Count != 1)
+            {
+                throw new ValidationException("Wrong variant count");
+            }
+
+            if (question.Guesses.Any(x => x.UserId == userId && x.GuessUserId == guessUserId))
+            {
+                throw new ValidationException("Guess already saved");
+            }
+
+            var questionVariants = question.Variants.Where(x => questionVariantIds.Contains(x.Id)).ToList();
+
+            return new Guess
+            {
+                QuestionId = question.Id,
+                UserId = userId,
+                QuestionVariants = questionVariants,
+                GuessUserId = guessUserId
+            };
+        }
     }
 }
